50f77ed1af084266574764db410038a682059633
Eky Kwan <eky@eky.hk>
2019-04-03 15:20:10 +0800

    📦 Update built-in-element to @ungap/custom-elements-builtin, and fix some bugs in IE

--- src/example/main.js
+++ src/example/main.js
@@ -5,30 +5,32 @@ qrCodeImage.addEventListener('generated', event => {
 
 const fields = document.querySelectorAll('[name]');
 Array.from(fields).forEach(field => {
-	field.addEventListener('input', event => {
-		const {
-			name,
-			value,
-		} = event.target;
+	['input', 'change'].forEach(eventName => {
+		field.addEventListener(eventName, event => {
+			const {
+				name,
+				value,
+			} = event.target;
 
-		switch (name) {
-		case 'colorlight':
-		case 'colordark': {
-			qrCodeImage.setAttribute(name, value + 'ff');
-			break;
-		}
-		case 'cover': {
-			const file = event.target.files[0];
-			const fileReader = new FileReader();
-			fileReader.onload = () => {
-				qrCodeImage.setAttribute(name, fileReader.result);
-			};
-			fileReader.readAsDataURL(file);
-			break;
-		}
-		default: {
-			qrCodeImage.setAttribute(name, value);
-		}
-		}
-	}, false);
+			switch (name) {
+				case 'colorlight':
+				case 'colordark': {
+					qrCodeImage.setAttribute(name, value + 'ff');
+					break;
+				}
+				case 'cover': {
+					const file = event.target.files[0];
+					const fileReader = new FileReader();
+					fileReader.onload = () => {
+						qrCodeImage.setAttribute(name, fileReader.result);
+					};
+					fileReader.readAsDataURL(file);
+					break;
+				}
+				default: {
+					qrCodeImage.setAttribute(name, value);
+				}
+			}
+		}, false);
+	});
 });
--- src/polyfills/index.js
+++ src/polyfills/index.js
@@ -1,8 +1,5 @@
 import '@babel/polyfill';
-
-// Need both customElements polyfill
-import '@webcomponents/webcomponentsjs';
 import 'document-register-element';
 
-// Use 'built-in-element' instead of es5 version if you don't need IE (*IE*, not Edge) support
-import './built-in-element-es5.js';
+// Use '@ungap/custom-elements-builtin' instead of es5 version if you don't need IE (*IE*, not Edge) support
+import './custom-elements-builtin-es5.js';
